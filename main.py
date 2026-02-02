f462daec6caa23428d6b6545d673965d9a082ac1
macadhn <underprintdhn@gmail.com>
2026-02-02 17:20:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.uix.button import Button
 
 class ContoUP(App):
     def build(self):
-        return Label(text='Benvenuti in ContoUP!')
+        # Un bottone gigante che occupa tutta la schermata
+        return Button(text='ContoUP Attivo!\nPremi qui per iniziare', font_size=30)
 
 if __name__ == '__main__':
     ContoUP().run()
