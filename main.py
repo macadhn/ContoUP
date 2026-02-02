4f4033c41b9e0eabb692b26e2d718db4157a5490
macadhn <underprintdhn@gmail.com>
2026-02-02 17:07:35 +0100

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from kivy.app import App
-from kivy.uix.label import Label
-
-class ContoUP(App):
-    def build(self):
-        return Label(text='Benvenuti in ContoUP!\nPronto per NFC')
-
-if __name__ == '__main__':
-    ContoUP().run()
